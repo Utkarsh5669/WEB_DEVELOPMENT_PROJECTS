60cf4c3e6e06914842bf6fd637bccfbe2512b048
Utkarsh5669 <116023139+Utkarsh5669@users.noreply.github.com>
2024-03-02 13:42:03 +0530

    Add files via upload

---
+++ J Query/index.js
@@ -0,0 +1,12 @@
+// $("h1").css("color", "red");
+
+// $("h1"); //selecting one header element
+// $("button");  //selecting all button 
+// in java query one and all selected with same way
+
+$("h1").addClass("big_title margin-50");
+
+// $("h1").removeClass("big_title");  //for removoing
+// $("h1").hasClass("big_title");  //for checking if class is there or not.
+
+$("a").attr("href", "https://yahoo.com");
